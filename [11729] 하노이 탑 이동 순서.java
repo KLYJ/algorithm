6812cd9fd4c6f034188f976447a0cf1402728da2
KLYJ <lyjl1006@naver.com>
2021-02-07 16:42:40 +0900

    Create [11729] 하노이 탑 이동 순서.java

---
+++ [11729] 하노이 탑 이동 순서.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class BJ_11729 {
+
+	static int answer = 0;
+    static StringBuilder sb = new StringBuilder();
+    
+    private static void hanoi(int n, int from, int temp, int to){
+        if(n==0) return;
+        
+        hanoi(n-1, from, to, temp);
+        sb.append(from+" "+to+"\n");
+        answer++;
+        hanoi(n-1, temp, from, to);        
+    }
+    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+        
+        hanoi(n, 1, 2, 3);
+        
+        System.out.println(answer);
+        System.out.println(sb.toString());
+        
+    }
+	
+}
