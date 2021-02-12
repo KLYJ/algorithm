94ca36fec59279f2be1049faf7e43061b52375eb
KLYJ <lyjl1006@naver.com>
2021-02-12 11:37:25 +0900

    Create 3진법 뒤집기.java

---
+++ 3진법 뒤집기.java
@@ -0,0 +1,32 @@
+package Level1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _3진법뒤집기 {
+	
+	public static void main(String[] args) {
+		System.out.println(solution(45));
+		System.out.println(solution(125));
+	}
+	
+	public static int solution(int n) {
+		List<Integer> list = new ArrayList<>();
+		//10진법->3진법 뒤집어서 list에 저장
+		while(n!=0) {
+			list.add(n%3);
+			n /= 3;
+		}
+		
+		//3진법 -> 10진법
+        int answer = 0;
+		int num=1;
+		for(int i=list.size()-1;i>=0;i--) {
+			answer += list.get(i)*num;
+			num *= 3;
+		}
+		
+        return answer;
+    }
+
+}
