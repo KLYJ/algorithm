f625e108509662a0e42b109f77f7cac0cf5b464a
KLYJ <lyjl1006@naver.com>
2021-02-15 23:31:05 +0900

    Create 완주하지 못한 선수.java

---
+++ 완주하지 못한 선수.java
@@ -0,0 +1,54 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class 완주하지못한선수 {
+	
+	public static void main(String[] args) {
+		System.out.println(solution2(new String[] {"leo", "kiki", "eden"}, new String[] {"eden", "kiki"}));
+	}
+	
+	public String solution(String[] participant, String[] completion) {
+        //answer에 <참가자 이름, 수> 저장
+        Map<String, Integer> answer = new HashMap<>();
+        
+        for(int i=0;i<participant.length;i++){
+            String name = participant[i];
+            int people = 1;
+            if(answer.containsKey(name)){
+                people += answer.get(name);
+            }
+            answer.put(name, people);
+        }
+        
+        //참가자와 완주자 비교
+        for(String name:completion){
+            int people = answer.get(name);
+            if(people == 1){
+                answer.remove(name);
+            }
+            else{
+                answer.put(name, people-1);
+            }
+        }
+        
+        //완주하지 못한 선수(=answer에 남은 선수) 리턴 
+        return answer.keySet().iterator().next();
+    }
+	
+	public static String solution2(String[] participant, String[] completion) {
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+		String answer = "";
+		
+		for(int i=0;i<completion.length;i++) {
+			if(!participant[i].equals(completion[i])) {
+				return participant[i];
+			}
+		}
+
+		return participant[participant.length-1];
+
+	}
+	
+}
